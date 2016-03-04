44a0d9647a782348316cbf623221539052cf5c7d
Jacopo <teslah2o@work>
2016-03-04 15:38:03 +0100

    added option to pass extra arguments to setup.sh file for pip command

--- setup.sh
+++ setup.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
-
+# Pass pip extra options as script arguments
+XOPTS=''
+if [[ "$#" > "0" ]]; then
+    XOPTS="${@:1}"
+fi
 # Change to scrpt directory
 cd "$(dirname "$0")"
 echo "Change to script directory" 
 # First install scipy (required by scikit-learn)
 echo "First install scipy (required by scikit-learn)"
-pip install scipy==0.16.0
+pip install ${XOPTS} scipy==0.16.0
 
 # Install required packages from online repositories and
 # from local directory for nolearn
 echo "Install required packages from online repositories and from local directory for nolearn"
-pip install -r cnn/requirements.txt 
-pip install -e cnn/src/nolearn
+pip install ${XOPTS} -r cnn/requirements.txt 
+pip install ${XOPTS} -e cnn/src/nolearn
 
 # Change to original directory
 echo "DONE. Change back to the original directory"
